297eb1535f3b48c2d088cf6f99bced46faaef5e7
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2022-02-16 11:57:16 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import FavList from "./components/FavList";
 
 function App() {
+  const [favoriteList,SetFavList] = useState([]);
+
+
+const fetchData = async () => {
+  try{
+    const res = await fetch(`${process.env.REACT_APP_SERVER}/favRecipes`);
+
+    const data = await res.json();
+    SetFavList(data);
+
+  }catch (error) {
+    console.log("error", error);
+  }
+
+}
+
+
+  useEffect(() => {
+    fetchData();
+  } , []);
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favList" element={<FavList favoriteList={favoriteList} />} />
+      </Routes>
+    </>
   );
 }
+
 
 export default App;
